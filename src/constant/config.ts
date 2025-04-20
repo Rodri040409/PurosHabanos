12888d5c19daba85f7cfbcc931a4386564517c52
Rodri040409 <rodrigo.ruiz.aguilar.cbtis165@gmail.com>
2025-04-19 22:37:22 -0600

    Detalles de personalización

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -2,5 +2,5 @@ export const siteConfig = {
   title: 'Puros Orquídea',
   description:
     'Descubre la elegancia del buen fumar. Puros artesanales seleccionados con el más alto estándar de calidad y sabor excepcional.',
-  url: 'https://purosorquidea.com', // Reemplázalo por tu dominio real si ya lo tienes
+  url: 'https://puros-habanos.vercel.app', // Reemplázalo por tu dominio real si ya lo tienes
 };
